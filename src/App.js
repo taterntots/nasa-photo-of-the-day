c6329030c285dc856a540fb6b578c9b4038f94b5
taterntots <matthew.a.weidner@gmail.com>
2019-11-06 15:20:25 -0500

    Got a couple data points to display on the page, including the image.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import React from "react";
+import React from 'react';
 import DailyPhoto from './components/DailyPhoto';
-import "./App.css";
+import './App.css';
 
 function App() {
   return (
--- src/components/DailyPhoto.js
+++ src/components/DailyPhoto.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 export default function DailyPhoto() {
@@ -8,8 +8,8 @@ export default function DailyPhoto() {
         axios
             .get('https://api.nasa.gov/planetary/apod?api_key=DESCjsuuPZBfTZSIzfMBNAiIKVD8LvPcoatRSyID')
             .then(response => {
-                setPhoto(response);
-                console.log(response);
+                setPhoto(response.data);
+                console.log(response.data);
             })
             .catch(error => {
                 console.log("The data hates your guts.", error);
@@ -18,7 +18,9 @@ export default function DailyPhoto() {
 
     return(
         <div className='photo'>
-            
+            <h2>{photo.date}</h2>
+            <img className='dailyImage' alt='space stuff' src={photo.hdurl}/>
+            <p>{photo.explanation}</p>
         </div>
     )
 }
